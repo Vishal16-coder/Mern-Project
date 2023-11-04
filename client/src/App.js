a2df7f5e7b86c690001591f0aa0815458bc9e81a
Vishal udhani <vishaludhani16@gmail.com>
2023-11-04 23:23:47 +0530

    Made the setup for frontend and login page

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+//components
+import Login from './component/account/login';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Login/>
+    </div>
+  );
+}
+
+export default App;
